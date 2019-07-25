45ace69c93e7a88603a7e6a21440c5c38d09fdb1
Yayun (Kim) Yang <hxjkim92@gmail.com>
2019-07-25 17:55:34 -0400

    Q7 Completed

---
+++ LeetCode/Q7_ReverseInteger.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class Q7_ReverseInteger
+    {
+        /*
+            Given a 32-bit signed integer, reverse digits of an integer.
+
+            Example 1:
+            Input: 123
+            Output: 321
+
+            Example 2:
+            Input: -123
+            Output: -321
+
+            Example 3:
+            Input: 120
+            Output: 21
+         */
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Reverse(123));
+            Console.WriteLine(Reverse(-123));
+            Console.WriteLine(Reverse(120));
+        }
+
+        public static int Reverse(int x)
+        {
+            if (x == 0) return 0; 
+            bool flag = x > 0; //To determine input is positive number or negative number
+            int value = flag ? x : -x;
+            long result = 0;
+            while (value > 0)
+            {
+                int num = value % 10;
+                value = value / 10;
+                result = result * 10 + num;
+                if (result > int.MaxValue) return 0;
+            }
+
+            return (int)(flag ? result : -result);
+        }
+    }
+}
