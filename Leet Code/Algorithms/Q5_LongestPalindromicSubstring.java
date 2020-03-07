8a62a96127346140331b475c740c262be67eb54b
Yayun Yang <hxjkim92@gmail.com>
2020-03-06 23:20:00 -0500

    update and formatting

--- Leet Code/Algorithms/Q5_LongestPalindromicSubstring.java
+++ Leet Code/Algorithms/Q5_LongestPalindromicSubstring.java
@@ -13,28 +13,28 @@ Output: "bb"
 
  */
 
- class Solution {
+class Solution {
     public String longestPalindrome(String s) {
-        if (s==null || s.length() <1) return "";
-        int startIndex = 0;
-        int endIndex = 0;
-        for(int i =  0; i < s.length(); i++){
-            int lengt1 = expand(s, i, i);
-            int lengt2 = expand(s, i, i+1);
-            int maxLength = Math.max(lengt1, lengt2);
-            if(maxLength > (endIndex - startIndex)){
-                startIndex = i - (maxLength-1)/2;
-                endIndex = i + maxLength/2;
-            }
-        }
-        return s.substring(startIndex, endIndex+1);
-    }
-    
-    public int expand(String s, int left, int right){
-        while(left>0&&right<s.length()&&s.charAt(left)==s.charAt(right)){
-            left--;
-            right++;
-        }        
-        return right-left-1;
-    }
+		if (s == null || s.length() < 1)
+			return "";
+		int startIndex = 0, endIndex = 0;
+		for (int i = 0; i < s.length(); i++) {
+			int length1 = expand(s, i, i);
+			int length2 = expand(s, i, i + 1);
+			int maxLength = Math.max(length1, length2);
+			if (maxLength > (endIndex - startIndex)) {
+				startIndex = i - (maxLength - 1) / 2;
+				endIndex = i + maxLength / 2;
+			}
+		}
+		return s.substring(startIndex, endIndex + 1);
+	}
+
+	public int expand(String s, int left, int right) {
+		while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+			left--;
+			right++;
+		}
+		return right - left - 1;
+	}
 }
