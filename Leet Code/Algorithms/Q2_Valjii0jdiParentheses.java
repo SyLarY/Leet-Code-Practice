af5072ac67d64db63a7d9604326bff64b7178fb7
Yayun Yang <hxjkim92@gmail.com>
2020-03-11 01:38:34 -0400

    Update Q2_Valjii0jdiParentheses.java

--- Leet Code/Algorithms/Q2_Valjii0jdiParentheses.java
+++ Leet Code/Algorithms/Q2_Valjii0jdiParentheses.java
@@ -29,6 +29,9 @@ Output: true
 
  */
 
+
+//Runtime: 1ms
+
 class Solution {
 	public boolean isValid(String s) {
         Map<Character, Character> maps = new HashMap<>();
@@ -36,21 +39,43 @@ class Solution {
         maps.put('}','{');
         maps.put(']','[');
         
-        Stack stack = new Stack();
+        Stack<Character> stack = new Stack<Character>();
         char[] ch = s.toCharArray();
-        char top = '';
         
         for(char c : ch){
             if(maps.containsKey(c)){
-                 top = stack.empty() ? '#' : stack.pop();
-            if(top != maps.get())
-                return false;
+                char top = stack.empty() ? '#' : stack.pop();
+                if(top != maps.get(c))
+                 return false;
             }else{
                 stack.push(c);
-            }
-               return stack.isEmpty();
-            
-                
+            }           
         }
+        return stack.isEmpty();
+    }
+}
+
+//Approach 2: 
+    //for open bracket in string -> push its close bracket to stack instead
+    //for close bracket in string -> pop the top element of stack and check if they are the same. 
+//Runtime: 1ms
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<Character>();
+        for(char c : s.toCharArray()){
+            if(c =='('){
+                stack.push(')');
+            }
+            else if( c =='{'){
+                stack.push('}');
+            }
+            else if(c == '['){
+                stack.push(']');
+            }
+            else if(stack.isEmpty() || stack.pop() != c){
+                return false;
+            }
+        }
+        return stack.isEmpty();
     }
 }
