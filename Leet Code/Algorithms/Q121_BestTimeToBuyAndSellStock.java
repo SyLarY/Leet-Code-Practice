02bfd12851e13ba00fdf79287c2cf097118e5e2f
Yayun Yang <hxjkim92@gmail.com>
2020-05-23 01:21:44 -0400

    Update Q121_BestTimeToBuyAndSellStock.java

--- Leet Code/Algorithms/Q121_BestTimeToBuyAndSellStock.java
+++ Leet Code/Algorithms/Q121_BestTimeToBuyAndSellStock.java
@@ -1,12 +1,13 @@
 /*
 Say you have an array for which the ith element is the price of a given stock on day i.
 
-If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+If you were only permitted to complete at most one transaction 
+(i.e., buy one and sell one share of the stock), 
+design an algorithm to find the maximum profit.
 
 Note that you cannot sell a stock before you buy one.
 
 Example 1:
-
 Input: [7,1,5,3,6,4]
 Output: 5
 Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
@@ -19,7 +20,7 @@ Explanation: In this case, no transaction is done, i.e. max profit = 0.
  */
 
 
-
+//Brute Force
  //Runtime: 310ms
 
 class Solution {
@@ -32,6 +33,26 @@ class Solution {
                int profit = prices[j] - prices[i];
                if(profit > maxProfit)
                    maxProfit = profit;
+           }
+        }
+        
+        return maxProfit;
+        
+    }
+}
+
+//One Pass
+//Runtime: 1ms
+class Solution {
+    public int maxProfit(int[] prices) {
+        int min = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        
+        for(int i = 0; i < prices.length; i++){
+           if(prices[i] < min){
+                min = prices[i];
+           }else if(prices[i] - min > maxProfit){
+               maxProfit = prices[i] - min;
            }
         }
         
